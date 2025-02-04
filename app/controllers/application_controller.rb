6374c304695b77eaddd4be5d0ff457877295809c
s-kishimoto <sarii.4u@icloud.com>
2025-02-04 22:48:30 +0900

    React Native設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+  def noop
+    render plain: 'Hello, World!'
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,6 @@ module HealthLogApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.exceptions_app = self.routes
   end
 end
